46534641597c9800a3aa88689cff7b6beaf695c1
vladimir <grun_gespenst@tut.by>
2020-12-20 17:59:38 +0300

    4 exersize

--- a_bigest_number.py
+++ a_bigest_number.py
@@ -1,9 +1,9 @@
 def bigest_number(lst):
-    another_list = sorted([str(i) for i in lst], reverse=True)
+    another_list = sorted([str(i) for i in lst], key=lambda i:len(i), reverse=False)
     max_number=''
     for i in another_list:
         max_number+=i
     print(max_number)
 
-lst=[1, 9, 44, 102, 7, 102]
+lst=[1, 12, 7777777]
 bigest_number(lst)
