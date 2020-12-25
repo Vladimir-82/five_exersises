a1f52f965b3226d881d58fb5cb39f637d8083396
vladimir <grun_gespenst@tut.by>
2020-12-25 11:36:08 +0300

    процедура находит максимальное число, составленное из чисел в списке

--- a_bigest_number.py
+++ a_bigest_number.py
@@ -1,9 +1,18 @@
+from itertools import permutations as per
 def bigest_number(lst):
-    another_list = sorted([str(i) for i in lst], key=lambda i:len(i), reverse=False)
-    max_number=''
-    for i in another_list:
-        max_number+=i
-    print(max_number)
+    """
+    процедура находит максимальное число, составленное из чисел в списке
+    :param lst:
+    :return:
+    """
+    res_list=[]
+    string_list=[str(i) for i in lst]
+    for i in per(string_list):
+        strig=''
+        for j in i:
+            strig+=j
+            res_list.append(strig)
+    print(max(res_list[(len(lst)-1)::len(lst)]))
 
-lst=[1, 12, 7777777]
+lst=[1, 10, 77, 2, 4, 3]
 bigest_number(lst)
